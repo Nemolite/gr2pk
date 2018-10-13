afc02fb473a39925c10754282872899be968d65e
Nemolite <g25092011@mail.ru>
2018-10-13 16:51:15 +0300

    gr2pk-fixing

---
+++ date/4/site/js/script.js
@@ -0,0 +1,12 @@
+var rooffloor = ("___________");
+var wall = ("|4444|3333|");
+var center=("|----+----|");
+
+
+console.log(rooffloor);
+console.log(wall);
+console.log(wall);
+console.log(center);
+console.log(wall);
+console.log(wall);
+console.log(rooffloor);
